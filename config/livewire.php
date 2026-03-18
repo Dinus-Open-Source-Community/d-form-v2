82f6d6f94ede59d70407a03a74d176cd45adb918
mbagusaditya <bagusaditya.dev@gmail.com>
2026-03-18 15:15:31 +0700

    🔧 Fix: filament config for artisan make command

--- config/livewire.php
+++ config/livewire.php
@@ -14,7 +14,7 @@ return [
     */
 
     'component_locations' => [
-        resource_path('views/components'),
+        // resource_path('views/components'),
         resource_path('views/livewire'),
     ],
 
@@ -69,7 +69,7 @@ return [
     */
 
     'make_command' => [
-        'type' => 'sfc', // Options: 'sfc', 'mfc', 'class'
+        'type' => 'class', // Options: 'sfc', 'mfc', 'class'
         'emoji' => false, // Options: true, false
         'with' => [
             'js' => false,
