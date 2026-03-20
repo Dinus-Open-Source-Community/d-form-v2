0d1d37fe46fcce022114154aa4d294450f9ceebb
mbagusaditya <bagusaditya.dev@gmail.com>
2026-03-20 21:44:23 +0700

    🔧 Fix: sorting logic in events list page, responsive bug in auth page, and buttons box display bug in trashed event

--- resources/views/components/module/event/show/buttons-box.blade.php
+++ resources/views/components/module/event/show/buttons-box.blade.php
@@ -3,16 +3,9 @@
 ])
 
 <div class="border-base-300 grid grid-cols-2 gap-4 rounded-lg md:grid-cols-1 md:border md:p-6 xl:grid-cols-2">
-    <a href="{{ route('dashboard.events.edit', $event['id']) }}" class="btn btn-warning btn-soft group md:relative">
-        @svg('heroicon-o-pencil-square', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
-        <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
-            {{ __('Edit') }}
-        </span>
-    </a>
-
     @if ($event->deleted_at !== null)
         <button
-            class="btn btn-success btn-soft group relative"
+            class="btn btn-success btn-soft group relative col-span-2 md:col-span-1 xl:col-span-2"
             x-on:click="$wire.mountAction('restore')"
             wire:key="restore-btn-{{ $event->id }}"
         >
@@ -22,6 +15,16 @@
             </span>
         </button>
     @else
+        <a
+            href="{{ route('dashboard.events.edit', $event['id']) }}"
+            class="btn btn-warning btn-soft group md:relative"
+        >
+            @svg('heroicon-o-pencil-square', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
+            <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
+                {{ __('Edit') }}
+            </span>
+        </a>
+
         <button
             class="btn btn-error btn-soft group relative"
             x-on:click="$wire.mountAction('delete')"
@@ -32,21 +35,21 @@
                 {{ __('Delete') }}
             </span>
         </button>
+
+        <button class="btn btn-primary btn-soft group relative">
+            @svg('heroicon-o-arrow-down-tray', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
+            <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
+                {{ __('Export') }}
+            </span>
+        </button>
+
+        <button class="btn btn-primary btn-soft group relative">
+            @svg('heroicon-o-arrow-up-tray', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
+            <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
+                {{ __('Import') }}
+            </span>
+        </button>
     @endif
-
-    <button class="btn btn-primary btn-soft group relative">
-        @svg('heroicon-o-arrow-down-tray', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
-        <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
-            {{ __('Export') }}
-        </span>
-    </button>
-
-    <button class="btn btn-primary btn-soft group relative">
-        @svg('heroicon-o-arrow-up-tray', 'size-[1.4em] opacity-100 transition-all duration-300 md:absolute lg:group-hover:opacity-0')
-        <span class="transition-all duration-300 md:opacity-0 lg:group-hover:opacity-100">
-            {{ __('Import') }}
-        </span>
-    </button>
 
     {{--
         <button class="btn btn-success btn-soft group relative">
