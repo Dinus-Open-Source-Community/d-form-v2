4c071ccaa380cb62e1cd7854f4d562616a4cb176
mbagusaditya <bagusaditya.dev@gmail.com>
2026-04-20 21:27:05 +0700

    chore: stop tracking actions, routes, and wayfinder in resources/js

--- resources/js/actions/App/Http/Controllers/Dashboard/Events/Forms/index.ts
+++
@@ -1,9 +0,0 @@
-import FormController from './FormController'
-import FieldOperationController from './FieldOperationController'
-
-const Forms = {
-    FormController: Object.assign(FormController, FormController),
-    FieldOperationController: Object.assign(FieldOperationController, FieldOperationController),
-}
-
-export default Forms
--- resources/js/actions/App/Http/Controllers/Dashboard/Events/index.ts
+++
@@ -1,9 +0,0 @@
-import EventController from './EventController'
-import Forms from './Forms'
-
-const Events = {
-    EventController: Object.assign(EventController, EventController),
-    Forms: Object.assign(Forms, Forms),
-}
-
-export default Events
